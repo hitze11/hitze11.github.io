8601a30d541172d8b04afcb35edba07f12058e3e
hitze <hitze1126@googlemail.com>
2025-03-24 08:57:56 +0100

    styling

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
+import 'dotenv/config';
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/config', (req, res) => {
   res.json({
