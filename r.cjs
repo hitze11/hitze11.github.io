5eaf36034ff6a235fab21a5d644d37cc1892eef8
hitze <hitze1126@googlemail.com>
2025-03-27 12:22:02 +0100

    testetet

--- r.cjs
+++
@@ -1,5 +0,0 @@
-const CryptoJS = require('crypto-js');
-
-// Generieren eines zufälligen 16-Byte-Schlüssels
-const secretKey = CryptoJS.lib.WordArray.random(16).toString();
-console.log("Generated Secret Key:", secretKey);
